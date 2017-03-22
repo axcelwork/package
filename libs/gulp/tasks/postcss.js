ba95a9eea4e4a4b0da32f054d52c0472995b56f0
axcelwork <yamaji@firstbrand.co.jp>
2017-03-22 14:25:00 +0900

    PostCSS入れてみた

---
+++ libs/gulp/tasks/postcss.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp"),
+    gulpif = require("gulp-if"),
+    plumber = require("gulp-plumber"),
+    cache = require('gulp-cached'),
+    browserSync = require('browser-sync'),
+
+    postcss = require('gulp-postcss'),
+    cssnext = require('postcss-cssnext'),
+
+    config = require("../config");
+
+
+gulp.task('pcss', function() {
+  var processors = [ cssnext(config.autoprefixer) ];
+
+  return gulp
+  .src([config.paths.pcss])
+  .pipe(cache( 'pcss' ))
+  .pipe(plumber())
+  .pipe(postcss(processors))
+  .pipe(gulp.dest(config.paths.base));
+});
